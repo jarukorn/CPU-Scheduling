42cd871ad64109208bffbd91853fd48a0829169c
Jarukorn Thuengjitvilas <jarukornmamee@gmail.com>
2018-09-26 00:19:19 +0700

    Initial Commit

---
+++ CPU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CPU
+//
+//  Created by Jarukorn Thuengjitvilas on 26/9/2561 BE.
+//  Copyright © 2561 Jarukorn Thuengjitvilas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
